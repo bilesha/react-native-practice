58ed1fee0d39c1dcbfecfd836d5efe76927819be
Bilesha Gunasekara <73975745+bilesha@users.noreply.github.com>
2025-07-02 22:22:03 +1200

    create post page using mutation

--- convex/schema.ts
+++ convex/schema.ts
@@ -21,7 +21,8 @@ export default defineSchema({
     storageId: v.id("_storage"),
     caption: v.optional(v.string()),
     likes: v.number(),
-    comments: v.number(),  
+    comments: v.number(),
+    imageUrl: v.string(),  
   }).index("by_user", ["userId"]),
 
   likes: defineTable({
